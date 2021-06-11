daac700258f84db67e24e161582df69486eed235
Sophie Strittmatter <sophie.strittmatter@gmail.com>
2021-06-11 13:57:18 +0200

    Kommentar hinzugefügt

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav';
 const Layout = ({ children }) => {
     return (
         <>
-            /* Weiteres Flex muss außen sein mit "row" damit der Content neben der Navigationsleiste angezeigt wird, und nicht darunter*/
+            {/* Weiteres Flex muss außen sein mit "row" damit der Content neben der Navigationsleiste angezeigt wird, und nicht darunter*/}
             <Flex direction="row">
                 <Nav />
                 <Flex direction="column" padding="8">

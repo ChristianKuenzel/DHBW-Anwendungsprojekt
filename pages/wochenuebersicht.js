510c58ec11cdc9a024b4cfa2e705f78fae05771c
Strittso <83401118+Strittso@users.noreply.github.com>
2021-06-20 22:29:10 +0200

    Update wochenuebersicht.js

--- pages/wochenuebersicht.js
+++ pages/wochenuebersicht.js
@@ -1,5 +1,3 @@
-import DhbwButton from '../components/Button';
-import Searchbar from '../components/Searchbar';
 import Texteingabe from '../components/Texteingabe';
 
 export default function wochenübersicht() {

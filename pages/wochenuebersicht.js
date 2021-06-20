8dc40fa0f2f38624e77e213aba518be75141cd41
Sophie Strittmatter <sophie.strittmatter@gmail.com>
2021-06-20 22:47:22 +0200

    test

--- pages/wochenuebersicht.js
+++ pages/wochenuebersicht.js
@@ -1,11 +1,15 @@
 import DhbwButton from '../components/Button';
 import Searchbar from '../components/Searchbar';
 import Texteingabe from '../components/Texteingabe';
+import VorlesungHinzufuegenKursplan from '../components/VorlesungHinzufuegenKursplan';
 
 export default function wochenübersicht() {
     return (
-        <Texteingabe placeholder="eingeben" width="100px" size="sm"/>
-       
+        <div>
+            <Texteingabe placeholder="eingeben" width="100px" size="sm"/>
+            <VorlesungHinzufuegenKursplan />
+        </div>
+        
         
 
     );

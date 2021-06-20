3e8a3a9af8ab1faa1202e14e70149bc28f87cf80
Strittso <83401118+Strittso@users.noreply.github.com>
2021-06-20 22:30:15 +0200

    Update wochenuebersicht.js

--- pages/wochenuebersicht.js
+++ pages/wochenuebersicht.js
@@ -1,10 +1,1 @@
-import Texteingabe from '../components/Texteingabe';
 
-export default function wochenübersicht() {
-    return (
-        <Texteingabe placeholder="eingeben" width="100px" size="sm"/>
-       
-        
-
-    );
-}

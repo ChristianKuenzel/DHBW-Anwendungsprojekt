20b782f710c74b6d04e04f413ecf495d7be0d6fc
leonard-kipar <83407850+leonard-kipar@users.noreply.github.com>
2021-06-20 15:32:06 +0200

    Add files via upload

--- pages/vorlesungsverzeichnis.js
+++ pages/vorlesungsverzeichnis.js
@@ -1,5 +1,39 @@
-import Textfeld from '../components/Textfeld';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Popup } from '../components/Popup';
 
-export default function wochenübersicht() {
-    return <Textfeld />;
-}
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const Button = styled.button`
+    min-width: 50px;
+    max-width: 300px;
+    padding: 5px 10px;
+    border-radius: 100px;
+    background: #E3001B;
+    color: #fff;
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    cursor: pointer;
+`;
+
+function Vorlesungsverzeichnis() {
+    const [showPopup, setShowPopup] = useState(false);
+  
+    const openPopup = () => {
+      setShowPopup(prev => !prev);
+    };
+  
+    return (
+        <Container>
+          <Button onClick={openPopup}>Vorlesung erstellen</Button>
+          <Popup showPopup={showPopup} setShowPopup={setShowPopup} />
+        </Container>
+    );
+  }
+  
+  export default Vorlesungsverzeichnis;
